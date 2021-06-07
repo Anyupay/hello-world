e3c7415ffa729a627a2d17e62b6b483b78620b01
Anyupay <anyupay@outlook.com>
2021-06-07 15:00:46 +0800

    First Commit

---
+++ GitDemo/GitDemo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GitDemo
+//
+//  Created by 潘安宇 on 2021/6/7.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
